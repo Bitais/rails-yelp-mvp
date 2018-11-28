5d67011ded4da9aa80a13a5e63cd289e2e1176fa
Thibaut de Briey <tdebriey@gmail.com>
2018-11-28 20:40:39 +0000

    first commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: "Sushis awesome", address: "10 rue awesome, Paris, FRANCE", category: "chinese")
+Restaurant.create(name: "pizza amazing", address: "10 rue extreme, Brussels, BELGIUM", category: "italian")
+Restaurant.create(name: "Eggs wtf", address: "10 rue amazing, Rome, ITALY", category: "japanese")
+Restaurant.create(name: "At john's", address: "10 rue super, Lisbon, PORTUGAL", category: "japanese")
+Restaurant.create(name: "Cafe Suzan", address: "10 rue Bad, London, UK", category: "french")
